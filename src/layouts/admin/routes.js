04d3addbbffd12d2be3f0c7b63ca3bc446e08990
Shun Hui <56659883+Shun-97@users.noreply.github.com>
2022-08-11 18:50:18 +0800

    Recommendation Done

--- src/layouts/admin/routes.js
+++ src/layouts/admin/routes.js
@@ -18,12 +18,13 @@ import SignInCentered from "views/auth/signIn";
 // Common import
 import Home from "views/common/home";
 import Report from "views/common/report";
+import recommendation from "views/common/recommendation";
 
 const routes = [
   {
     name: "Main",
     layout: "/admin",
-    path: "/default",
+    path: "/main",
     icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
     component: Home,
   },
@@ -37,9 +38,16 @@ const routes = [
   {
     name: "Report",
     layout: "/admin",
-    path: "/profile",
+    path: "/report",
     icon: <Icon as={MdPerson} width='20px' height='20px' color='inherit' />,
     component: Report,
+  },
+  {
+    name: "Recommendation",
+    layout: "/admin",
+    path: "/recommendation",
+    icon: <Icon as={MdPerson} width='20px' height='20px' color='inherit' />,
+    component: recommendation,
   }
 ];
 
---
+++ src/views/common/recommendation/index.jsx
@@ -0,0 +1,128 @@
+// Chakra imports
+import {
+    Box,
+    useColorModeValue,
+    Text,
+    Flex,
+    Spacer,
+    FormLabel,
+    Select,
+    Switch
+} from "@chakra-ui/react";
+
+// Custom components
+import Card from "components/card/Card.js";
+import React from "react";
+import { useEffect } from "react";
+
+
+export default function Recommendation() {
+    // Chakra Color Mode
+    const brandColor = useColorModeValue("brand.500", "white");
+    const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
+    const textColorPrimary = useColorModeValue("secondaryGray.900", "white");
+    const textColorSecondary = "gray.400";
+    const cardShadow = useColorModeValue(
+        "0px 18px 40px rgba(112, 144, 176, 0.12)",
+        "unset"
+    );
+
+
+    const [typeTop, settypeTop] = React.useState("Most");
+    const [dataList, setDataList] = React.useState([]);
+    const [industry, setIndustry] = React.useState("");
+    const [allData, setAllData] = React.useState([]);
+
+    useEffect(() => {
+        fetch("/db/industry_output.json")
+            .then(res => res.json())
+            .then((data) => {
+                setAllData(data)
+                setDataList(data.sort((a, b) => b.profileScore < a.profileScore ? 1 : -1))
+
+            })
+    }, [])
+
+
+
+    const changeSwitch = (e) => {
+        let switching = typeTop
+        switching === "Most" ? settypeTop("Top") : settypeTop("Most");
+        if (switching === "Top") {
+            setDataList(dataList.sort((a, b) => b.profileScore < a.profileScore ? 1 : -1))
+        } else {
+            setDataList(dataList.sort((a, b) => b.total < a.total ? 1 : -1))
+        }
+    }
+
+    const changeIndustry = (e) => {
+        setIndustry(e.target.value)
+    }
+    
+
+    return (
+        <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+            <Flex flexDirection='column' mx='3rem'>
+                <Flex
+                    direction='row-reverse'
+                    me='20px'
+                    ms="2rem"
+                    mt="2rem"
+                    mb="2rem">
+
+                    <FormLabel htmlFor='isChecked'>Top Performing</FormLabel>
+                    <Switch mx="1rem" id='isChecked' onChange={(e) => changeSwitch(e)} />
+                    <FormLabel htmlFor='isChecked'>Most Relevant</FormLabel>
+
+                    <Spacer />
+                    <Select onChange={(e) => changeIndustry(e)} variant='filled' placeholder='Filter by industry' w='200px' >
+                        <option value='Transport'>Transport</option>
+                        <option value='OilAndGas'>OilAndGas</option>
+                        <option value='Technology'>Technology</option>
+                        <option value='AerospaceAndDefence'>AerospaceAndDefence</option>
+                        <option value='Food'>Food</option>
+                    </Select>
+                </Flex>
+
+                <Flex flexDirection={'row'} overflowX='scroll'>
+                    <Flex>
+                        <Text w='8rem' mx='1.5rem' textAlign={'center'}>Industry</Text>
+                        <Text w='10rem' mx='1.5rem'>Company Name</Text>
+                    </Flex>
+                    <Spacer />
+                    <Flex>
+                        <Text w='7rem' mx='1.5rem' textAlign={'center'}>Environmental</Text>
+                        <Text w='5.5rem' mx='1.5rem' textAlign={'center'}>Governance</Text>
+                        <Text w='7rem' mx='1.5rem' textAlign={'center'}>Social</Text>
+                        <Text w='7rem' mx='1.5rem' textAlign={'center'}>Performance</Text>
+                    </Flex>
+                </Flex>
+
+                {(industry === "" 
+                ?dataList
+                :dataList.filter(item => item.industry === industry)
+                ).map((data, key) => {
+                    return (
+                        <Card
+                            my='0.5rem'
+                            key={key}>
+                            <Flex flexDirection={'row'}>
+                                <Flex>
+                                    <Text w='8rem' mx='1.5rem' textAlign={'center'}>{data.industry}</Text>
+                                    <Text w='10rem' ml='2.5rem'>{data.name}</Text>
+                                </Flex>
+                                <Spacer />
+                                <Flex>
+                                    <Text w='7rem' mx='1rem' textAlign={'center'}>{data.env}</Text>
+                                    <Text w='7rem' mx='1rem' textAlign={'center'}>{data.gov}</Text>
+                                    <Text w='7rem' mx='1rem' textAlign={'center'}>{data.soc}</Text>
+                                    <Text w='7rem' mx='1rem' textAlign={'center'}>{data.total}</Text>
+                                </Flex>
+                            </Flex>
+                        </Card>
+                    )
+                })}
+            </Flex>
+        </Box >
+    );
+}
