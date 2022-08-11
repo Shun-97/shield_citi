b8445949621538236d1e482c048bbfbf01664395
Brayden Leo <46068652+prussianman@users.noreply.github.com>
2022-08-11 17:18:35 +0800

    esg calculation done

--- backend/complexMicroservices/esgCalculation.py
+++ backend/complexMicroservices/esgCalculation.py
@@ -22,6 +22,51 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 # service classes to use
-Portfolio_URL = "http://localhost:5003"
-Preference_URL = "http://localhost:5002"
-Product_URL = "http://localhost:5001"
+CUSTOMER_URL = "http://localhost:5003/"
+EQUITY_URL = "http://localhost:5006/"
+ESG_PROFILING_URL = "http://localhost:5004/"
+
+@app.route("/recommendations", methods=['POST'])
+def recommendations():
+
+
+    if request.is_json:
+        # try:
+        data = request.get_json()
+        cid = data['cid']
+        print("\nReceived customer ID in JSON:", cid)
+        result = processRecommendation(cid)
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "equity": result
+                }
+            }
+        ), 200
+
+def processRecommendation(cid):
+
+    response1 = invoke_http(ESG_PROFILING_URL + "EsgScore/"+str(cid), method='GET')
+    env = response1['data']['env']
+    soc = response1['data']['soc']
+    gov = response1['data']['gov']
+
+
+    response2 = invoke_http(method='GET', url=EQUITY_URL + "equity")
+    equityList = response2['data']['equity']
+    result = []
+
+    for equity in equityList:
+        print(equity)
+        equity = json.dumps(equity)
+        equityDict = json.loads(equity)
+        print(env)
+        print(equityDict['env'])
+        equityDict['profileScore'] = round(((env/100) * equityDict['env'] + (soc/100) * equityDict['soc'] + (gov/100) * equityDict['gov']) * 3, 2) 
+        result.append(equityDict)
+    
+    return result
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5198, debug=True)
