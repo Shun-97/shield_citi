75fd66312d5e88573aeb3ecc7f533a1d2739aee3
Brayden Leo <46068652+prussianman@users.noreply.github.com>
2022-08-12 00:11:31 +0800

    stahed changes

--- backend/db/Companies.sql
+++ backend/db/Companies.sql
@@ -1,36 +1,62 @@
-
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+08:00";
-
+SET time_zone
+= "+08:00";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-DROP DATABASE IF EXISTS `COMPANIES`;
-CREATE DATABASE `COMPANIES`;
-USE `COMPANIES`;
-CREATE TABLE `COMPANIES` (
-  `ticker` varchar(50) NOT NULL,
-  `name` varchar(120) NOT NULL,
-  `industry` varchar(120) NOT NULL,
-  `price` float DEFAULT NULL,
-  `total` float DEFAULT NULL,
-  `env` float DEFAULT NULL,
-  `soc` float DEFAULT NULL,
-  `gov` float DEFAULT NULL,
-  PRIMARY KEY (ticker)
+DROP DATABASE IF EXISTS `CUSTOMER`;
+CREATE DATABASE `CUSTOMER`;
+USE `CUSTOMER`;
+
+
+DROP TABLE IF EXISTS `NEW_CUST_KYC`;
+CREATE TABLE `NEW_CUST_KYC` (
+    `cid` int NOT NULL,
+    `name` varchar(99) NOT NULL,
+    `risk_Appetite` int NOT NULL,
+    PRIMARY KEY(cid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-INSERT INTO `COMPANIES` (`ticker`, `name`, `industry`, `price`, `total`, `env`, `soc`, `gov`) VALUES
-('LUV', 'Southwest Airlines Co.', 'Airlines', '38.34', '32', '11.3', '14.9', '6.2'),
-('AAPL', 'Apple Inc.', 'Technology', '164.92', '16', '0.6', '6.9', '8.9'),
-('GOOGL', 'Alphabet Inc.', 'Technology', '116.63', '1.7', '11.1', '11.5');
+INSERT INTO `NEW_CUST_KYC` (`cid`, `name`, `risk_appetite`) VALUES
+(1, 'Alex', 50),
+(2, 'Bobby', 45),
+(3, 'Charlie', 30),
+(4, 'Denise', 25),
+(5, 'Edward', 55);
 
+DROP TABLE IF EXISTS `EXISTING_CUST_PORTFOLIO`;
+CREATE TABLE `EXISTING_CUST_PORTFOLIO`
+(
+    `cid` int NOT NULL,
+    `ticker` varchar(50) NOT NULL,
+    `qty` int(11) NOT NULL,  
+    PRIMARY KEY(cid, ticker)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO `EXISTING_CUST_PORTFOLIO` (`cid`, `ticker`, `qty`) VALUES
+('1', 'AAPL', 85),
+('1', 'GOOGL', 3),
+('1', 'NFLX34.SA', 100);
+
+DROP TABLE IF EXISTS `NEW_CUST_PREFERRED_INDUSTRY`;
+CREATE TABLE `NEW_CUST_PREFERRED_INDUSTRY`
+(
+    `cid` varchar(50) NOT NULL,
+    `preferred_industry` varchar(50) NOT NULL
+    
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `NEW_CUST_PREFERRED_INDUSTRY` (`cid`, `preferred_industry`) VALUES
+(1, 'Technology'),
+(1, 'Oil and Gas'),
+(1, 'Biotechnology');
 
 COMMIT;
 
--- backend/misc/result.py
+++ backend/misc/result.py
@@ -3,3 +3,4 @@ Biotechnology = ['AMGN', 'LIFE', 'CSL.AX', 'BIIB', 'BMRN', 'TECH', 'UCB.BR', 'IN
 Technology = ['GOOGL', 'AKAM', 'REA.AX', 'BIDU', 'FB', 'Z', 'ZG', 'TWTR', 'VRSN', 'JD', 'TRIP', 'JDN.MX', '0700.HK', 'YNDX', 'BIDUN.MX', 'TWTR.MX', 'B1C.F', 'NTESN.MX', 'IAC', 'B1C.DU']
 AerospaceAndDefence = ['BOE.L', 'BA.L', 'PSK.TO', 'NOC', 'MTX.DE', 'S63.SI', 'SAF.PA', 'HO.PA', 'RR.L', 'TXT', 'NTH.SG', 'NTH.F', 'NTH.DU', 'BA.MX', 'BCO.BE', 'MTXN.MX', 'BSP.DU', 'BCO.DE', 'GD.MX', 'LMT.MX', 'LOM.MU', 'NTH.BE', 'TXT.MX', 'UTC1.HA']
 Food = ['ORK.OL', 'ABF.L', 'BUFF', 'BIMBOA.MX', 'BN.PA', 'JBSS3.SA', 'KHC', 'GRUMAB.MX', 'DNN.MI', 'KHC.MX', '2319.HK', '0322.HK', '0151.HK', 'GRM.MU', 'GV.PA', 'BNN.MX', 'BRFS3.SA', 'BSN.HA', '0QR4.L', 'NESR.HA', 'GRM.SG', 'GLB.L', 'KYGA.L', 'NESR.MU', 'OKL.BE', 'SAP.TO', '1216.TW']
+
